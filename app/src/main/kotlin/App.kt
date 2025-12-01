a35a78164e142c734000a84a201885a40041cf0d
Zablon Dawit <zabjd22@gmail.com>
2025-12-01 15:44:56 +0300

    feat: write response header

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -1,15 +1,33 @@
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.InputStreamReader
 import java.net.ServerSocket
 
 fun main(args: Array<String>) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.err.println("Logs from your program will appear here!")
 
-    var serverSocket = ServerSocket(9092)
+    val serverSocket = ServerSocket(9092)
 
-     // Since the tester restarts your program quite often, setting SO_REUSEADDR
-     // ensures that we don't run into 'Address already in use' errors
-     serverSocket.reuseAddress = true
+    //region SocketOptions
+    // Since the tester restarts your program quite often, setting SO_REUSEADDR
+    // ensures that we don't run into 'Address already in use' errors
+    serverSocket.reuseAddress = true
+    //endregion
 
-     serverSocket.accept() // Wait for connection from client.
-     println("accepted new connection")
+    while(true) {
+        val socket = serverSocket.accept()
+        socket.use { client ->
+            val inputStream = BufferedReader(InputStreamReader(client.getInputStream()))
+            val outputStream = DataOutputStream(client.getOutputStream())
+
+            inputStream.readLine() // Read the request line
+
+            outputStream.writeInt(0)
+            outputStream.writeInt(7)
+            outputStream.flush()
+
+            println("accepted new connection")
+        }
+    }
 }
